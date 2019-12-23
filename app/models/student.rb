5daf7c4f6527b2ca88997d63de9fe3c53f4270ba
Ronald Stephens <rstephens@bluemorpho.cc>
2019-12-23 00:42:34 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ActiveRecord::Base
 
   def name
     Student.all do |student|
-      @student.to_s = student.first_name
+      @student.to_s = student(params[:first_name])
     end
   end
 end
