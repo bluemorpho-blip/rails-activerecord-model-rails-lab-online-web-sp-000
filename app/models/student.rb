fd21b8d8a9b6e627cc351dffd75daf650b11a9ac
Ronald Stephens <rstephens@bluemorpho.cc>
2019-12-23 00:25:19 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,8 @@ class Student < ActiveRecord::Base
     Student.all do |student|
       @name = "#{student.first_name} #{student.last_name}"
       @name = @name.to_s
+
+      t.timestamps null: false
     end
   end
 end
